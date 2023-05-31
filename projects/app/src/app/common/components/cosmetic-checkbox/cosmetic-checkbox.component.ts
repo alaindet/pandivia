011643ea7fdc7e5649bab44ebb92cb3f093237a4
Alain D'Ettorre <alain.det@gmail.com>
2023-05-31 09:19:25 +0200

    WIP

--- projects/app/src/app/common/components/cosmetic-checkbox/cosmetic-checkbox.component.ts
+++ projects/app/src/app/common/components/cosmetic-checkbox/cosmetic-checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy, Input, HostBinding, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, ViewEncapsulation } from '@angular/core';
 
 type CosmeticCheckboxColor = 'primary' | 'secondary' | 'tertiary' | 'black';
 
@@ -12,17 +12,34 @@ type CosmeticCheckboxColor = 'primary' | 'secondary' | 'tertiary' | 'black';
   styleUrls: ['./cosmetic-checkbox.component.scss'],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'app-cosmetic-checkbox' },
+  host: {
+    class: 'app-cosmetic-checkbox',
+    role: 'checkbox',
+  },
 })
-export class CosmeticCheckboxComponent implements OnInit {
-  
-  @Input() @HostBinding('class.-checked') checked = false;
-  @Input() color: CosmeticCheckboxColor = 'primary';
-  @Input() @HostBinding('style.--app-cosmetic-checkbox-size') size = '20px';
+export class CosmeticCheckboxComponent {
 
-  @HostBinding('class') cssClasses!: string;
+  @Input()
+  @HostBinding('class.-checked')
+  @HostBinding('attr.aria-checked')
+  checked = false;
 
-  ngOnInit() {
-    this.cssClasses = `-color-${this.color}`;
+  @Input()
+  @HostBinding('style.--app-cosmetic-checkbox-size')
+  size = '20px';
+
+  @Input()
+  set color(val: CosmeticCheckboxColor) {
+    this.cssClasses = `-color-${val}`;
   }
+
+  @Input()
+  set isInteractable(val: boolean) {
+    this.cssIsInteractable = val;
+    this.tabIndex = val ? '0' : '-1';
+  }
+
+  @HostBinding('class') cssClasses = '-color-primary';
+  @HostBinding('class.-interactable') cssIsInteractable = true;
+  @HostBinding('tabindex') tabIndex = '0';
 }
