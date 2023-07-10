0e85f39f5323d612e240a8aa85eabd5310cc2c25
Alain D'Ettorre <alain.det@gmail.com>
2023-07-11 00:08:20 +0200

    Add CRUD with Firebase to InventoryService

--- projects/app/src/app/features/inventory/services/inventory.service.ts
+++ projects/app/src/app/features/inventory/services/inventory.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable, from } from 'rxjs';
 import { Store } from '@ngrx/store';
-import { Firestore, addDoc, collection, getDoc } from '@angular/fire/firestore';
+import { CollectionReference, DocumentData, DocumentSnapshot, Firestore, QuerySnapshot, addDoc, collection, doc, getDoc, getDocs, updateDoc, deleteDoc } from '@angular/fire/firestore';
 
 import { selectUserId } from '@app/features/user/store';
 import { CreateInventoryItemDto, InventoryItem } from '../types';
@@ -15,19 +15,59 @@ export class InventoryService {
   private store = inject(Store);
   private userId = this.store.selectSignal(selectUserId);
 
+  getItems(): Observable<InventoryItem[]> {
+    return from((async () => {
+      const itemsRef = this.getItemsRef();
+      const docs = await getDocs(itemsRef);
+      return this.docsToInventoryItems(docs);
+    })());
+  }
+
   createItem(dto: CreateInventoryItemDto): Observable<InventoryItem> {
+    return from((async () => {
+      const itemsRef = this.getItemsRef();
+      const itemRef = await addDoc(itemsRef, dto);
+      const itemDoc = await getDoc(itemRef);
+      return this.docToInventoryItem(itemDoc);
+    })());
+  }
+
+  editItem(editedItem: InventoryItem): Observable<InventoryItem> {
+    return from((async () => {
+      const userId = this.userId()!;
+      const itemId = editedItem.id;
+      const itemRef = doc(this.db, 'invites', userId, 'items', itemId);
+      const { id: _, ...dto } = editedItem;
+      await updateDoc(itemRef, dto);
+      return editedItem;
+    })());
+  }
+
+  removeItem(itemId: InventoryItem['id']): Observable<InventoryItem> {
+    return from((async () => {
+      const userId = this.userId()!;
+      const itemRef = doc(this.db, 'invites', userId, 'items', itemId);
+      const itemDoc = await getDoc(itemRef);
+      await deleteDoc(itemRef);
+      return this.docToInventoryItem(itemDoc);
+    })());
+  }
+
+  private getItemsRef(): CollectionReference<DocumentData> {
 
     if (!this.userId()) {
       throw new Error('No user ID');
     }
 
-    return from((async () => {
-      const userId = this.userId()!;
-      const itemsRef = collection(this.db, 'inventories', userId, 'items');
-      const itemRef = await addDoc(itemsRef, dto);
-      const itemDoc = await getDoc(itemRef);
-      const item = { ...itemDoc.data(), id: itemRef.id } as InventoryItem;
-      return item;
-    })());
+    const userId = this.userId()!;
+    return collection(this.db, 'inventories', userId, 'items');
+  }
+
+  private docToInventoryItem(doc: DocumentSnapshot<DocumentData>): InventoryItem {
+    return { ...doc.data(), id: doc.id } as InventoryItem;
+  }
+
+  private docsToInventoryItems(docs: QuerySnapshot<DocumentData>): InventoryItem[] {
+    return docs.docs.map(doc => this.docToInventoryItem(doc));
   }
 }
