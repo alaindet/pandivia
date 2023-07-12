155bf33c99b8218dfb5229f1c88d86e628e3c53e
Alain D'Ettorre <alain.det@gmail.com>
2023-07-12 12:14:34 +0200

    WIP: Refactor list actions

--- projects/app/src/app/features/list/store/reducer.ts
+++ projects/app/src/app/features/list/store/reducer.ts
@@ -3,128 +3,167 @@ import { immerOn } from 'ngrx-immer/store';
 
 import { LOADING_STATUS } from '@app/common/types';
 import { LIST_FILTER } from '../types';
-import {
-  listItemsAsyncReadActions as itemsAsyncReadActions,
-  listAllItemsActions as allItemsActions,
-  listCategoryActions as categoryActions,
-  listItemActions as itemActions,
-  listItemsAsyncWriteActions as itemsAsyncWriteActions,
-  listItemAsyncWriteActions as itemAsyncWriteActions,
-  listFilterActions as filterActions,
-} from './actions';
 import { LIST_FEATURE_INITIAL_STATE } from './state';
 import { setSuccessState } from './helpers';
+import { listChangeItemAmount, listCompleteItem, listCompleteItems, listCompleteItemsByCategory, listCreateItem, listEditItem, listFetchItems, listRemoveCompletedItems, listRemoveCompletedItemsByCategory, listRemoveItem, listRemoveItems, listRemoveItemsByCategory, listToggleItem } from './actions';
 
+// export type ListFeatureState = {
+//   items: ListItem[];
+//   status: LoadingStatus;
+//   lastUpdated: UnixTimestamp | null;
+//   itemModalSuccessCounter: number;
+//   filters: ListFilters;
+// };
 export const listReducer = createReducer(LIST_FEATURE_INITIAL_STATE,
 
-  // Generic async ------------------------------------------------------------
-
   immerOn(
-    itemsAsyncReadActions.fetchItems,
-    itemsAsyncReadActions.forceFetchItems,
-
-    allItemsActions.complete,
-    allItemsActions.undo,
-    allItemsActions.removeCompleted,
-    allItemsActions.remove,
-
-    categoryActions.complete,
-    categoryActions.undo,
-    categoryActions.removeCompleted,
-    categoryActions.remove,
-
-    itemActions.complete,
-    itemActions.create,
-    itemActions.decrement,
-    itemActions.edit,
-    itemActions.increment,
-    itemActions.remove,
-    itemActions.toggle,
-    itemActions.undo,
+    listFetchItems.try,
+    listFetchItems.force,
+    listCompleteItems.try,
+    listRemoveItems.try,
+    listRemoveCompletedItems.try,
+    listCompleteItemsByCategory.try,
+    listRemoveCompletedItemsByCategory.try,
+    listRemoveItemsByCategory.try,
+    listCreateItem.try,
+    listEditItem.try,
+    listCompleteItem.try,
+    listToggleItem.try,
+    listChangeItemAmount.try,
+    listRemoveItem.try,
     state => {
       state.status = LOADING_STATUS.LOADING;
     },
   ),
 
   immerOn(
-    itemsAsyncReadActions.fetchItemsError,
-
-    itemsAsyncWriteActions.editError,
-    itemsAsyncWriteActions.removeError,
-
-    itemAsyncWriteActions.createError,
-    itemAsyncWriteActions.editError,
-    itemAsyncWriteActions.removeError,
+    listFetchItems.err,
+    listCompleteItems.err,
+    listRemoveItems.err,
+    listRemoveCompletedItems.err,
+    listCompleteItemsByCategory.err,
+    listRemoveCompletedItemsByCategory.err,
+    listRemoveItemsByCategory.err,
+    listCreateItem.err,
+    listEditItem.err,
+    listCompleteItem.err,
+    listToggleItem.err,
+    listChangeItemAmount.err,
+    listRemoveItem.err,
     state => {
       state.status = LOADING_STATUS.ERROR;
     },
   ),
 
-  // Fetching -----------------------------------------------------------------
 
-  immerOn(itemsAsyncReadActions.fetchItemsCached, state => {
-    state.status = LOADING_STATUS.IDLE;
-  }),
+  // Generic async ------------------------------------------------------------
 
-  immerOn(itemsAsyncReadActions.fetchItemsSuccess, (state, { items }) => {
-    setSuccessState(state);
-    state.items = items;
-  }),
+  // immerOn(
+  //   itemsAsyncReadActions.fetchItems,
+  //   itemsAsyncReadActions.forceFetchItems,
 
-  // Filters ------------------------------------------------------------------
+  //   allItemsActions.complete,
+  //   allItemsActions.undo,
+  //   allItemsActions.removeCompleted,
+  //   allItemsActions.remove,
 
-  immerOn(filterActions.setCategoryFilter, (state, { category }) => {
-    state.filters[LIST_FILTER.CATEGORY] = category;
-  }),
+  //   categoryActions.complete,
+  //   categoryActions.undo,
+  //   categoryActions.removeCompleted,
+  //   categoryActions.remove,
 
-  immerOn(filterActions.clearCategoryFilter, state => {
-    state.filters[LIST_FILTER.CATEGORY] = null;
-  }),
+  //   itemActions.complete,
+  //   itemActions.create,
+  //   itemActions.decrement,
+  //   itemActions.edit,
+  //   itemActions.increment,
+  //   itemActions.remove,
+  //   itemActions.toggle,
+  //   itemActions.undo,
+  //   state => {
+  //     state.status = LOADING_STATUS.LOADING;
+  //   },
+  // ),
 
-  immerOn(filterActions.setDoneFilter, (state, { isDone }) => {
-    state.filters[LIST_FILTER.IS_DONE] = isDone;
-  }),
+  // immerOn(
+  //   itemsAsyncReadActions.fetchItemsError,
 
-  immerOn(filterActions.clearDoneFilter, state => {
-    state.filters[LIST_FILTER.CATEGORY] = null;
-  }),
+  //   itemsAsyncWriteActions.editError,
+  //   itemsAsyncWriteActions.removeError,
 
-  immerOn(filterActions.clearAllFilters, state => {
-    state.filters[LIST_FILTER.CATEGORY] = null;
-    state.filters[LIST_FILTER.IS_DONE] = null;
-  }),
+  //   itemAsyncWriteActions.createError,
+  //   itemAsyncWriteActions.editError,
+  //   itemAsyncWriteActions.removeError,
+  //   state => {
+  //     state.status = LOADING_STATUS.ERROR;
+  //   },
+  // ),
 
-  immerOn(filterActions.clearFilterByName, (state, { name }) => {
-    state.filters[name] = null;
-  }),
+  // // Fetching -----------------------------------------------------------------
 
-  // List items async write ---------------------------------------------------
+  // immerOn(itemsAsyncReadActions.fetchItemsCached, state => {
+  //   state.status = LOADING_STATUS.IDLE;
+  // }),
 
-  immerOn(
-    itemsAsyncWriteActions.editSuccess,
-    itemsAsyncWriteActions.removeSuccess,
-    (state, { items }) => {
-      setSuccessState(state);
-      state.items = items;
-    },
-  ),
+  // immerOn(itemsAsyncReadActions.fetchItemsSuccess, (state, { items }) => {
+  //   setSuccessState(state);
+  //   state.items = items;
+  // }),
 
-  // List item async write ----------------------------------------------------
+  // // Filters ------------------------------------------------------------------
 
-  immerOn(itemAsyncWriteActions.createSuccess, (state, { item }) => {
-    setSuccessState(state);
-    state.itemModalSuccessCounter += 1;
-    state.items.unshift(item);
-  }),
+  // immerOn(filterActions.setCategoryFilter, (state, { category }) => {
+  //   state.filters[LIST_FILTER.CATEGORY] = category;
+  // }),
 
-  immerOn(itemAsyncWriteActions.editSuccess, (state, { item }) => {
-    setSuccessState(state);
-    state.itemModalSuccessCounter += 1;
-    state.items = state.items.map(anItem => anItem.id === item.id ? item : anItem);
-  }),
+  // immerOn(filterActions.clearCategoryFilter, state => {
+  //   state.filters[LIST_FILTER.CATEGORY] = null;
+  // }),
 
-  immerOn(itemAsyncWriteActions.removeSuccess, (state, { item }) => {
-    setSuccessState(state);
-    state.items = state.items.filter(anItem => anItem.id !== item.id);
-  }),
+  // immerOn(filterActions.setDoneFilter, (state, { isDone }) => {
+  //   state.filters[LIST_FILTER.IS_DONE] = isDone;
+  // }),
+
+  // immerOn(filterActions.clearDoneFilter, state => {
+  //   state.filters[LIST_FILTER.CATEGORY] = null;
+  // }),
+
+  // immerOn(filterActions.clearAllFilters, state => {
+  //   state.filters[LIST_FILTER.CATEGORY] = null;
+  //   state.filters[LIST_FILTER.IS_DONE] = null;
+  // }),
+
+  // immerOn(filterActions.clearFilterByName, (state, { name }) => {
+  //   state.filters[name] = null;
+  // }),
+
+  // // List items async write ---------------------------------------------------
+
+  // immerOn(
+  //   itemsAsyncWriteActions.editSuccess,
+  //   itemsAsyncWriteActions.removeSuccess,
+  //   (state, { items }) => {
+  //     setSuccessState(state);
+  //     state.items = items;
+  //   },
+  // ),
+
+  // // List item async write ----------------------------------------------------
+
+  // immerOn(itemAsyncWriteActions.createSuccess, (state, { item }) => {
+  //   setSuccessState(state);
+  //   state.itemModalSuccessCounter += 1;
+  //   state.items.unshift(item);
+  // }),
+
+  // immerOn(itemAsyncWriteActions.editSuccess, (state, { item }) => {
+  //   setSuccessState(state);
+  //   state.itemModalSuccessCounter += 1;
+  //   state.items = state.items.map(anItem => anItem.id === item.id ? item : anItem);
+  // }),
+
+  // immerOn(itemAsyncWriteActions.removeSuccess, (state, { item }) => {
+  //   setSuccessState(state);
+  //   state.items = state.items.filter(anItem => anItem.id !== item.id);
+  // }),
 );
