29cb3ba7ebda65548811a4cb80fd41de9f63c346
Alain D'Ettorre <alain.det@gmail.com>
2023-07-15 12:22:30 +0200

    Add Angular service worker to application

--- TODO.md
+++ TODO.md
@@ -2,9 +2,9 @@
 - [ ] Configure firebase.indexes.json
 - [ ] Setup security rules
 - [ ] Fix broken language when English is stored in local storage
+- [ ] Add favicon
 
 # Backlog
-- [ ] Transform app to PWA
 - [ ] Create show/hide password input
 - [ ] Add dark themes
 - [ ] Split app in libraries and applications inside Angular Workspace
@@ -13,3 +13,6 @@
 - [?] Add ESLint and Prettier
 - [ ] Accept input strings instead of importing TranslocoModule inside components
 - [ ] Use &lt;dialog&gt;
+- [ ] Add screenshots to README.md and manifest.json
+- [ ] Add app version on user page
+- [ ] Remove demo page from user page
--- projects/app/src/app/app.config.ts
+++ projects/app/src/app/app.config.ts
@@ -1,15 +1,20 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
 import { NGRX_PROVIDERS } from './core/config/ngrx';
 import { TRANSLOCO_PROVIDERS } from './core/config/transloco';
 import { FIREBASE_PROVIDERS } from './core/config/firebase';
+import { provideServiceWorker } from '@angular/service-worker';
 
 const CORE_PROVIDERS = [
   provideHttpClient(),
   provideRouter(APP_ROUTES),
+  provideServiceWorker('ngsw-worker.js', {
+    enabled: !isDevMode(),
+    registrationStrategy: 'registerWhenStable:30000',
+  }),
 ];
 
 export const appConfig: ApplicationConfig = {
@@ -18,5 +23,5 @@ export const appConfig: ApplicationConfig = {
     ...NGRX_PROVIDERS,
     ...TRANSLOCO_PROVIDERS,
     ...FIREBASE_PROVIDERS,
-  ]
+  ],
 };
