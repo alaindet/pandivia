b690300ea8389f281d0874260d3fbdb0d1290b24
Alain D'Ettorre <alain.det@gmail.com>
2025-08-13 18:52:43 +0200

    refactor: use self-closing tags where possible

--- projects/ui/components/form-field/form-field-hint/form-field-hint.component.ts
+++ projects/ui/components/form-field/form-field-hint/form-field-hint.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-form-field-hint',
-  template: `<ng-content></ng-content>`,
+  template: `<ng-content />`,
   styleUrl: './form-field-hint.component.css',
   host: { class: 'app-form-field-hint' },
   encapsulation: ViewEncapsulation.None,
